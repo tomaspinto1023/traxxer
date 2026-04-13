73313d9ae80bb6468d0f0b2d9c3382880a2cf4b5
msolot1023 <tomas.pinto.contacto@gmail.com>
2026-04-13 22:07:48 +0100

    added this file for later

---
+++ frontend/js/trk1player.js
@@ -0,0 +1,2 @@
+//Este ficheiro é responsável por gerir funções de utilização dentro do deck uma vez que a musica estiver carregada!
+//Passar lógicas de ações que estão erradamente no uploadtracks.js!
